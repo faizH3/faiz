9afb7e393b14980e095f7cebf51f0569df746495
faizH3 <59959639+faizH3@users.noreply.github.com>
2020-11-14 11:01:44 +0700

    Program_kalkulator.py

--- Program_kalkulator.py
+++ Program_kalkulator.py
@@ -11,12 +11,12 @@ while(jawab=="y"):
   a=(A)
   b=(A)
   if a==1:
-    a1=raw_input("ketikan alas segitiga:")
-    a2=raw_input("ketikan tinggi segitiga:")
+    a1=int(input("ketikan alas segitiga:"))
+    a2=int(input("ketikan tinggi segitiga:"))
     Luas=(a1*a2)/2
     print("Luas=",Luas)
   elif b==2:
-    b1=raw_input("ketikan jari-jari lingkaran:")
+    b1=int(input("ketikan jari-jari lingkaran:"))
     Luas=22/7*(b1**2)
     print("Luas=",Luas)
   jawab= input("apakah anda ingin mengulangi lagi? y/n -")
