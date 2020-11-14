f9d24b289819517eddeac8c4013a7391826561d3
faizH3 <59959639+faizH3@users.noreply.github.com>
2020-11-14 17:30:50 +0700

    kalkulator_sederhana.py

--- kalkulator_sederhana.py
+++ kalkulator_sederhana.py
@@ -9,23 +9,23 @@ while (jawab=="y"):
 	print("2. menghitung luas lingkaran               ")
 	print("3. menghitung luas bujur sangkar           ")
 	print("")
-	A = input("pilih menu : ")
+	A = int(input("pilih menu : "))
 	a = (A)
 	b = (A)
 	c = (A)
 	if a==1:
-		a1 = input("    ketikan alas segitiga:    ")
-		a2 = input("  ketikan tinggi segitiga:    ")
+		a1 = int(input("    ketikan alas segitiga:    "))
+		a2 = int(input("  ketikan tinggi segitiga:    "))
 		print("-------------------------------------  ")
 		luas = (a1 * a2) / 2
 		print("                   luas: ",luas)
 	elif b==2:
-		b1 = input("ketikan jari-jari lingkaran:    ")
+		b1 = int(input("ketikan jari-jari lingkaran:    "))
 		print("-------------------------------------  ")
 		luas = 22 / 7 * (b1**2)
 		print("                     luas: ",luas)
 	elif c==3:
-		c1 = input("ketikan sisi bujur sangkar:    ")
+		c1 = int(input("ketikan sisi bujur sangkar:    "))
 		luas = c1 ** 2
 		print("-------------------------------------  ")
 		print("                    luas: ",luas )
