c5fb0400b33c04bc85b5d8d62e45587d7ac0b7eb
faizH3 <20420026@student.binadarma.ac.id>
2021-06-16 23:57:41 -0400

    upload

---
+++ Python/2 string,print,datetime/1 string/1 string.py
@@ -0,0 +1,22 @@
+# string
+# string data berikutnya yang anda akan pelajari adalah string. strng digunakan
+# untuk menyimpan karakter, kata, kalimat hingga paragraph
+# contoh:
+# seperti ini:
+#
+# nama = 'cinta'
+# umur = '34'
+# ucapan = 'pagi selalu cerita yang baru'
+# website = 'https://faizh3.codes'
+#
+# dicontoh diatas, kita membuat variabel nama, umur, ucapan dan juga website
+# yang merupakan sebuah string.
+# string selalu dimulai dan diakhiri dengan tanda "" atau ''
+# perhatikan bahwa disini umur bukanlah angka karena menggunakan ""
+
+'''
+instruksi:
+buat variabel yang bernama tokoh dan berikan sebuah nama
+'''
+tokoh = 'Pandawa 5'
+print(tokoh)
---
+++ Python/2 string,print,datetime/1 string/2 string lagi.py
@@ -0,0 +1,10 @@
+# oke mari kita berlatih lagi tentang string
+'''
+instruksi:
+jawab semua variabel di samping menggunakan strings.
+'''
+negara = 'Indonesia'
+ibukota = 'Jakarta'
+motto = 'Bhineka Tunggal Ika'
+tanggal_kemerdekaan = '17 agustus 1945'
+print('negara {}, ibukota {}, motto {}, tanggal kemerdekaan {}'.format(negara,ibukota,motto,tanggal_kemerdekaan))
