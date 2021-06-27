993a537a816bc65fb0f59cd0f3fb6787b6786ddf
Faiz Hidayat <59959639+faizH3@users.noreply.github.com>
2021-06-27 08:16:00 -0400

    Update 2 string lagi.py

--- Python/2 string,print,datetime/1 string/2 string lagi.py
+++ Python/2 string,print,datetime/1 string/2 string lagi.py
@@ -1,36 +1,10 @@
-# # oke mari kita berlatih lagi tentang string
-# '''
-# instruksi:
-# jawab semua variabel di samping menggunakan strings.
-# '''
-# negara = 'Indonesia'
-# ibukota = 'Jakarta'
-# motto = 'Bhineka Tunggal Ika'
-# tanggal_kemerdekaan = '17 agustus 1945'
-# print('negara {}, ibukota {}, motto {}, tanggal kemerdekaan {}'.format(negara,ibukota,motto,tanggal_kemerdekaan))
-import pygame
-pygame.init()
-
-width = 800
-height = int(width * 0.8)
-
-screen = pygame.display.set_mode((width, height))
-
-bg = (255,255,255)
-
-# def back_gr():
-#     screen.fill(bg)
-
-run = True
-
-while run:
-    # back_gr()
-    screen.fill((255,255,255))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                exit()
-    pygame.display.update()
-pygame.quit()
+# oke mari kita berlatih lagi tentang string
+'''
+instruksi:
+jawab semua variabel di samping menggunakan strings.
+'''
+negara = 'Indonesia'
+ibukota = 'Jakarta'
+motto = 'Bhineka Tunggal Ika'
+tanggal_kemerdekaan = '17 agustus 1945'
+print('negara {}, ibukota {}, motto {}, tanggal kemerdekaan {}'.format(negara,ibukota,motto,tanggal_kemerdekaan))
